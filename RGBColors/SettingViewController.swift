46b281e4a2a068dc5eb2ec542bf707da8aa68bd9
anitagaga <anita.gagarina@tixr.com>
2023-12-07 18:38:45 -0800

    added another view controller

--- RGBColors/SettingViewController.swift
+++ RGBColors/SettingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class SettingViewController: UIViewController {
 
     // MARK: - IB Outlets
     @IBOutlet var mainColorView: UIView!
